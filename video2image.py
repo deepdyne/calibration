03cf351c10d842107df61b5b6d8f0a52d7bcbbf2
mgoldchild <mgoldchild@gmail.com>
2019-04-17 15:47:40 +0900

    Update video2image.py

--- video2image.py
+++ video2image.py
@@ -1,14 +1,18 @@
 import os
 import cv2
 
-vidcap = cv2.VideoCapture('2019-04-10 10:47:59output.avi')
+vidcap = cv2.VideoCapture('20190417_142912_edited.mp4')
 success,image = vidcap.read()
 count = 0
 path = 'frames'
 success = True
+fps = 30
 
 while success:
-  cv2.imwrite(os.path.join(path, "%s.png" % str(count).zfill(8)), image)
-  success,image = vidcap.read()
-  print('Read a new frame: ', success)
+  print(count)
+  if count % fps == 0:
+    cv2.imwrite(os.path.join(path, "%s.png" % str(count).zfill(8)), image)
+    success,image = vidcap.read()
+    print('Read a new frame: ', success)
+
   count += 1
